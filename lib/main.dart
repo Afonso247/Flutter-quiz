d89359ac7a91689f7ed2e74ff1d1481461c780e9
afonso.araujo <afonso.araujo@supertroco.com.br>
2025-09-26 21:13:02 -0300

    whoops! forgot to commit it first (first commit)

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            color: Color.fromARGB(255, 114, 23, 117),
+          ),
+          child: StartScreen(),
+        ),
+      ),
+    ),
+  );
+}
---
+++ lib/start_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Image(
+            image: AssetImage("assets/images/quiz-logo.png"),
+            width: 200,
+          ),
+          SizedBox(height: 80),
+          const Text(
+            "Vamos aprender sobre Flutter!",
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton.icon(
+            style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.black,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(40),
+            )),
+            onPressed: () {
+              debugPrint('yay!');
+            },
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text("Começar Quiz"),
+          ),
+        ],
+      ),
+    );
+  }
+}
