16419bafce69a8a63bf9870de957cc440dcaa1a5
afonso.araujo <afonso.araujo@supertroco.com.br>
2025-09-27 22:50:52 -0300

    feat: pequena estlizacao dos botoes

--- lib/components/custom_button.dart
+++ lib/components/custom_button.dart
@@ -12,17 +12,20 @@ class CustomButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
-        backgroundColor: const Color.fromARGB(255, 68, 18, 85),
-        foregroundColor: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(40),
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 5),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+          backgroundColor: const Color.fromARGB(255, 68, 18, 85),
+          foregroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(40),
+          ),
         ),
+        onPressed: customFunction,
+        child: Text(customText, textAlign: TextAlign.center),
       ),
-      onPressed: customFunction,
-      child: Text(customText),
     );
   }
 }
