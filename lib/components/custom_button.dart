9b5394a11da4b891fa5be727e1c22700ef846221
afonso.araujo <afonso.araujo@supertroco.com.br>
2025-09-27 21:40:28 -0300

    feat: criacao de CustomButton

---
+++ lib/components/custom_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String customText;
+  final void Function() customFunction;
+
+  const CustomButton({
+    super.key,
+    required this.customText,
+    required this.customFunction,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+        backgroundColor: const Color.fromARGB(255, 68, 18, 85),
+        foregroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(40),
+        ),
+      ),
+      onPressed: customFunction,
+      child: Text(customText),
+    );
+  }
+}
--- lib/screens/questions_screen.dart
+++ lib/screens/questions_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:quiz_app/components/custom_button.dart';
+
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
 
@@ -10,6 +12,17 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Text('Quiz');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Pergunta'),
+          const SizedBox(height: 20),
+          CustomButton(customText: 'Resposta 1', customFunction: () {}),
+          CustomButton(customText: 'Resposta 2', customFunction: () {}),
+          CustomButton(customText: 'Resposta 3', customFunction: () {}),
+      ]),
+    );
   }
 }
